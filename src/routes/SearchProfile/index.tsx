2cc15c4760c89e6d4c12f8ee585c87ff97a914cb
Lucas Danilo <lukastecno@gmail.com>
2024-03-21 16:11:33 -0300

    Updated SearchProfile and css

--- src/routes/SearchProfile/index.tsx
+++ src/routes/SearchProfile/index.tsx
@@ -16,10 +16,7 @@ type UserData = {
 export default function SearchProfile() {
 
     const [username, setUserName] = useState("");
-    const [userData, setUserData] = useState<UserData | null>(null);
-
-
-
+    const [userData, setUserData] = useState<UserData>();
 
     const handleSearch = () => {
         axios.get(`${BASE_URL_GITHUB}/${username}`)
@@ -37,27 +34,50 @@ export default function SearchProfile() {
 
 
     return (
-        <div className="container-profile-search">
-            <div className="profile-search">
-                <main>
-                    <h1>Encontre um perfil Github</h1>
-                </main>
 
-                <div className="form-container">
-                    <form>
-                        <input
-                            className="form-custom"
-                            placeholder="Usuário Github"
-                            value={username}
-                            onChange={handleInputChange}
-                        />
-                    </form>
-                </div>
-                <div className="button-profile-search">
-                    <Button text="Encontrar" onClick={handleSearch} />
+        <>
+            <div className="container-profile-search">
+                <div className="profile-search">
+                    <main>
+                        <h1>Encontre um perfil Github</h1>
+                    </main>
+                    <div className="form-container">
+                        <form>
+                            <input
+                                className="form-custom"
+                                placeholder="Usuário Github"
+                                value={username}
+                                onChange={handleInputChange}
+                            />
+                        </form>
+                    </div>
+                    <div className="button-profile-search">
+                        <Button text="Encontrar" onClick={handleSearch} />
+                    </div>
                 </div>
             </div>
-        </div>
+            {userData && (
+                <div className="user-data-container">
+                    <div className="user-data">
+                        <div>
+                            <img className="img-custom" src={userData.avatar_url} alt="Avatar" />
+                        </div>
+                        <div className="user-info">
+                            <div className="info-title">
+                                <h2>Informaçoes</h2>
+                            </div>
+                            <div className="container-input">
+                                <div className="input-custom"><p><strong>Perfil:</strong> <a href={userData.url}>{userData.url}</a></p></div>
+                                <div className="input-custom"><p><strong>Seguidores:</strong> {userData.followers}</p></div>
+                                <div className="input-custom"><p><strong>Localidade:</strong> {userData.location}</p></div>
+                                <div className="input-custom"><p><strong>Nome:</strong>{userData.name}</p></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+
     );
 
 }

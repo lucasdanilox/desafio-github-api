7174694bc7bf3a4502c0011319c527933bba8e4e
Lucas Danilo <lukastecno@gmail.com>
2024-03-21 00:43:15 -0300

    Connection with api

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,14 +2,15 @@ import './styles.css';
 
 type Props = {
     text: string;
+    onClick: () => void;
 }
 
-export default function Button({ text }: Props) {
+export default function Button({ text, onClick }: Props) {
 
     return (
 
         <div className="container-button">
-            <button type="submit" className="button-custom">{text}</button>
+            <button onClick={onClick} className="button-custom">{text}</button>
         </div>
     );
 
--- src/routes/SearchProfile/index.tsx
+++ src/routes/SearchProfile/index.tsx
@@ -1,8 +1,40 @@
 
+import { useState } from 'react';
 import Button from '../../components/Button';
 import './styles.css';
+import { BASE_URL_GITHUB } from '../../utils/system';
+import axios from 'axios';
+
+type UserData = {
+    url: string;
+    followers: number;
+    location: string;
+    name: string;
+    avatar_url: string;
+};
 
 export default function SearchProfile() {
+
+    const [username, setUserName] = useState("");
+    const [userData, setUserData] = useState<UserData | null>(null);
+
+
+
+
+    const handleSearch = () => {
+        axios.get(`${BASE_URL_GITHUB}/${username}`)
+            .then(response => {
+                console.log(response.data);
+                setUserData(response.data);
+            });
+    };
+
+
+    function handleInputChange(event: any) {
+        const value = event.target.value;
+        setUserName(value);
+    }
+
 
     return (
         <div className="container-profile-search">
@@ -10,14 +42,19 @@ export default function SearchProfile() {
                 <main>
                     <h1>Encontre um perfil Github</h1>
                 </main>
+
                 <div className="form-container">
                     <form>
-                        <input className="form-custom" placeholder="Usuário Github">
-                        </input>
+                        <input
+                            className="form-custom"
+                            placeholder="Usuário Github"
+                            value={username}
+                            onChange={handleInputChange}
+                        />
                     </form>
                 </div>
                 <div className="button-profile-search">
-                    <Button text="Encontrar" />
+                    <Button text="Encontrar" onClick={handleSearch} />
                 </div>
             </div>
         </div>
